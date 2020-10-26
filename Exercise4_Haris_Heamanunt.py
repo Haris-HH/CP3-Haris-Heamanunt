dbde7cf3e2a35d857dce0c0a25e87753312dce7a
Haris-HH <haris_are@outlook.com>
2020-10-26 15:24:44 +0900

    Created Exercise 4
    added variable of  Foundation English,General Business,Introduction to Computer Systems,Computer Programming and printed it

---
+++ Exercise4_Haris_Heamanunt.py
@@ -0,0 +1,9 @@
+FoundEng = 30.5
+GenBusiness = 61
+IntroToComSystems = 85.4
+ComProgram = 99.9
+print("---Your Score---")
+print("Foundation English    : ",FoundEng)
+print("General Business      : ",GenBusiness)
+print("Introduction to Computer Systems : ",IntroToComSystems)
+print("Computer Programming : ",ComProgram)
